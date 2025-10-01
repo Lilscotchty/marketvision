41d7302b20b968edf57a3b331c453740d04b28bd
Prince Boateng <pb7552212@gmail.com>
2025-10-01 16:17:07 +0000

    https://your_auth_domain/__/auth/handler?apiKey=AIzaSyDLfpHD6tKlxekkYLH6

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,7 @@ const nextConfig: NextConfig = {
   },
   env: {
     NEXT_PUBLIC_FINNHUB_API_KEY: process.env.FINNHUB_API_KEY,
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   }
 };
 
