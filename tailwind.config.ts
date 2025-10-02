b8093222459bc4d04f58e31512f43bec4b23d3df
Prince Boateng <pb7552212@gmail.com>
2025-10-02 11:44:24 +0000

    Now reduce the animation speed, to slowly slide down and up to exit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -88,10 +88,20 @@ export default {
             height: '0',
           },
         },
+        'toast-slide-in-down': {
+          from: { transform: 'translateY(-100%)', opacity: '0' },
+          to: { transform: 'translateY(0)', opacity: '1' },
+        },
+        'toast-slide-out-up': {
+          from: { transform: 'translateY(0)', opacity: '1' },
+          to: { transform: 'translateY(-100%)', opacity: '0' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'toast-slide-in-down': 'toast-slide-in-down 0.4s ease-out',
+        'toast-slide-out-up': 'toast-slide-out-up 0.3s ease-in',
       },
     },
   },
