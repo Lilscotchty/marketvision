ef0fec243745a71eec9f0f3360ee732d78053e39
Prince Boateng <pb7552212@gmail.com>
2025-10-27 09:20:16 +0000

    Try fixing this error: `Console Error: CRITICAL ERROR: Firebase configur

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -6,9 +6,9 @@ import { getAuth, type Auth, GoogleAuthProvider } from 'firebase/auth';
 // Your Firebase project's configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseapp.com`,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.appspot.com`,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
