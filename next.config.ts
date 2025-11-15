a4561e960af02aab13692a9b1429d4a4fc42c2e7
Prince Boateng <pb7552212@gmail.com>
2025-11-15 10:17:00 +0000

    ## Error Type Runtime Error
    ## Error Message
    Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 // next.config.ts
 import type {NextConfig} from 'next';
 
@@ -25,6 +26,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lnpyhrogsrpywoftosbg.supabase.co',
         port: '',
         pathname: '/**',
       }
